8492246cba8472800113bd2607a1c360d02159fc
Sanal Mishra <16-sanalmishra>
2022-05-18 08:54:23 +0530

    File Deleted

--- app1.js
+++
@@ -1,9 +0,0 @@
-// const file = require('fs');
-// file.writeFileSync("notes.txt" , "This is text from nodejs");
-// file.appendFileSync()
-var myutils = require('./01-intro/utils');
-// var n = "Sanal";
-var name  = myutils.name;
-var func = myutils.addM(3,4);
-console.log(func);
-console.log(name);
