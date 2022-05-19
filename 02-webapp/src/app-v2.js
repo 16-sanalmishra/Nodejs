74568527474e2bbab4db09ff68cc8d8df711c71d
Sanal Mishra <16-sanalmishra>
2022-05-19 10:59:45 +0530

    Partials

--- 02-webapp/src/app-v2.js
+++ 02-webapp/src/app-v2.js
@@ -1,13 +1,41 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // console.log(publicDirectoryPath);
-app.set('view engine' , 'hbs');
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather Report',
+    name: 'John Doe',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Us Dynamic',
+    name: 'RK',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help Page',
+    name: 'RK',
+    helpText: 'Welcome to the product support page',
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
@@ -15,23 +43,8 @@ app.get('/weather', (req, res) => {
     location: 'Chennai',
   });
 });
-app.get('/about' , (req,res) =>{
-res.render('about' , {
-  title: 'About Us', 
-  name:'Sanal'
-})
-})
-app.get('/help' , (req,res) =>{
-  res.render('help' , {
-    Greetings:'Welcome To HBS'
-  })
-  })
-  app.get('/index' , (req,res) =>{
-    res.render('index' , {
-      Text:'Welcome To Landing Page'
-    })
-    })
 
-app.listen(3000 , ()=>{
-    console.log("running");
+app.listen(3000, () => {
+  console.log('Server is up and running on port 3000');
+  // console.log(__dirname);
 });
