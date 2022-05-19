4313f99a5087cee486a094ab75f7dd0e6f76195d
Sanal Mishra <16-sanalmishra>
2022-05-19 11:08:08 +0530

    Styles Added

--- 02-webapp/src/app-v2.js
+++ 02-webapp/src/app-v2.js
@@ -44,6 +44,22 @@ app.get('/weather', (req, res) => {
   });
 });
 
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Max',
+    errorMessage: 'Help Page not found.',
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Max',
+    errorMessage: 'Page not found.',
+  });
+});
+
 app.listen(3000, () => {
   console.log('Server is up and running on port 3000');
   // console.log(__dirname);
